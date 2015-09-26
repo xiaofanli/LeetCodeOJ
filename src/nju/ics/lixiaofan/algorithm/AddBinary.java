c156ef2ad746afff3af84cedc2db033a80d866d8
lixiaofan <leslie.ido@gmail.com>
2015-09-26 14:46:53 +0800

    Add Binary acc in 288ms

---
+++ src/nju/ics/lixiaofan/algorithm/AddBinary.java
@@ -0,0 +1,38 @@
+package nju.ics.lixiaofan.algorithm;
+/*
+Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+ */
+public class AddBinary {
+	public static void main(String[] args) {
+		String a = "111", b = "1001";
+		AddBinary ab = new AddBinary();
+		System.out.println(ab.addBinary(a, b));
+	}
+
+    public String addBinary(String a, String b) {
+        char[] m = a.toCharArray();
+        char[] n = b.toCharArray();
+        int i = m.length-1, j = n.length-1;
+        char[] ans = i > j ? m : n;
+        int idx = Math.max(i, j);
+        int x, y, c = 0;
+        while(i >= 0 && j >= 0){
+        	x = m[i--] - '0';
+        	y = n[j--] - '0';
+        	ans[idx--] = (char) (x ^ y ^ c + '0');
+        	c = x + y + c > 1 ? 1 : 0;
+        }
+        char tmp = '0'+'1';
+        while(c > 0 && idx >= 0){
+        	c = ans[idx] -'0';
+        	ans[idx] = (char) (tmp - ans[idx]);
+        	idx--;
+        }
+        return c > 0 ? "1"+String.valueOf(ans) : String.valueOf(ans);
+    }
+}
