bedd4804934ca0582b074ef08cddc9bc27916ded
lixiaofan <leslie.ido@gmail.com>
2015-10-20 16:10:33 +0800

    Find the Duplicate Number AC in O(n) time and O(1) space (copied solution)

--- src/nju/ics/lixiaofan/algorithm/FindTheDuplicateNumber.java
+++ src/nju/ics/lixiaofan/algorithm/FindTheDuplicateNumber.java
@@ -1,5 +1,37 @@
 package nju.ics.lixiaofan.algorithm;
+/*
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
 
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+ */
 public class FindTheDuplicateNumber {
-
+	//The idea is the same as Linked List Cycle II
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int count = 0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            count++;
+        }while(slow != fast);
+        
+        slow = fast = 0;
+        for(int i = 0;i < count;i++)
+            fast = nums[fast];
+        
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return fast;
+    }
 }
